633dba3b91d1b605672b8861e0192bfac6949b5e
scrutinously <96753388+scrutinously@users.noreply.github.com>
2023-10-02 17:48:59 -0400

    Update farmStats.sh with %%

--- farmStats.sh
+++ farmStats.sh
@@ -19,7 +19,7 @@ if [ -f "${logFile}".1 ]; then
 else
 	echo "Harvester Stats:"
 fi
-echo ${eligible[@]} | awk -v "t=$total" 'BEGIN {RS = "/"}; {av=$1/t*100} {if ($1 != "") printf "%-3d %s %s: %8d | %.2f%\n",$2,$3,$4,$1,av}'
+echo ${eligible[@]} | awk -v "t=$total" 'BEGIN {RS = "/"}; {av=$1/t*100} {if ($1 != "") printf "%-3d %s %s: %8d | %.2f%%\n",$2,$3,$4,$1,av}'
 echo ${lookup[@]} | awk 'BEGIN {RS = "/"}; $1>0.5{c1++}; $1>1{c2++}; $1>5{c3++}; {l="Lookups Longer Than"};
         {sum+=$1}; END {printf "%-22s %5d\n%-22s %5d\n%-22s %5d\n%-22s %9f %s\n",l" 0.5s:",c1,"\033[33m"l" 1.0s:",c2,"\033[31m"l" 5.0s:",c3,"\033[37mAverage Lookup:",sum / NR,"sec"}'
 echo ${lookup[@]} | awk 'BEGIN {RS = "/"}; $1>max{max=$1}; END {printf "%-16s %10f %s\n","Longest Lookup:",max,"sec"}'
